85fdc5c8cc24af8993d93849af9d51a995ad8aa3
sid-more <siddheshmore42@example.com>
2024-02-03 20:43:18 +0530

    LinkedListexample added

---
+++ com.gold/LinkedlistExample/LinkedListMain.cs
@@ -0,0 +1,79 @@
+// void LinkedList()
+// {
+//     LinkedList myList = new LinkedList();
+
+//     // Insert elements into the linked list
+//     myList.Insert(3);
+//     myList.Insert(7);
+//     myList.Insert(12);
+//     myList.Insert(5);
+
+//     // Display the linked list
+//     Console.WriteLine("Linked List elements:");
+//     myList.Display();
+// }
+
+class LinkedList
+{
+    private Node head;
+
+    public LinkedList()
+    {
+        head = null!;
+    }
+
+    public void Insert(int data)
+    {
+        Node newNode = new Node(data);
+        newNode.Next = head;
+        head = newNode;
+    }
+
+    public void Display()
+    {
+        Node current = head;
+
+        while (current != null)
+        {
+            Console.Write(current.Data + " ");
+            current = current.Next;
+        }
+
+        Console.WriteLine();
+    }
+
+    public void RemoveDuplicates()
+    {
+        HashSet<int> uniqueElements = new HashSet<int>();
+        Node current = head;
+        Node previous = null!;
+
+        while (current != null)
+        {
+            if (uniqueElements.Contains(current.Data))
+            {
+                // Remove duplicate by adjusting pointers
+                previous.Next = current.Next;
+            }
+            else
+            {
+                uniqueElements.Add(current.Data);
+                previous = current;
+            }
+
+            current = current.Next;
+        }
+    }
+}
+
+class Node
+{
+    public int Data;
+    public Node Next;
+
+    public Node(int data)
+    {
+        Data = data;
+        Next = null!;
+    }
+}
--- com.gold/Program.cs
+++ com.gold/Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
+﻿partial class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Gold!");
+
+        // LinkedList();
+
+        LinkedList myList = new LinkedList();
+
+        // Insert elements into the linked list
+        myList.Insert(33);
+        myList.Insert(77);
+        myList.Insert(22);
+        myList.Insert(55);
+        myList.Insert(22);
+
+        // Display the linked list
+        Console.WriteLine("Linked List elements:");
+        myList.Display();
+
+        myList.RemoveDuplicates();
+
+        // Display the linked list after removing duplicates
+        Console.WriteLine("Linked List elements after removing duplicates:");
+        myList.Display();
+
+
+
     }
 }
